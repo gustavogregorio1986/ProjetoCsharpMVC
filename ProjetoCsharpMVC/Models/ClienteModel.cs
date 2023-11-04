7c0080e451bec00bcbac5461f8ee9ad0c47da1af
guga56 <gustavogregorio086@gmail.com>
2023-11-04 15:23:16 -0300

    construindo o miodel da apolicação no frontend

---
+++ ProjetoCsharpMVC/Models/ClienteModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoCsharpMVC.Models
+{
+    public class ClienteModel
+    {
+        public int IdCliente { get; set; }
+
+        public string NomeCliente { get; set; }
+
+        public string Funcao { get; set; }
+    }
+}
---
+++ ProjetoCsharpMVC/Models/EmpresaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoCsharpMVC.Models
+{
+    public class EmpresaModel
+    {
+        public int IdEmpresa { get; set; }
+
+        public string NomeEmoresa { get; set; }
+
+        public string Setor { get; set; }
+
+        public string Cnpj { get; set; }
+    }
+}
